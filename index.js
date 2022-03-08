c780c0dfc08164fbaf5f9a34f27f8363cae6e98d
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-03-08 14:10:38 -0500

    modificat el port

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const dotenv = require("dotenv")
+dotenv.config()
+
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
